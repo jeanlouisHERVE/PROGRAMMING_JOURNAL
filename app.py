e1cceeea3e2498c023a41d6346c05d5ad3a1e139
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-07-01 16:16:09 +0200

    others improvements for application

--- app.py
+++ app.py
@@ -15,17 +15,21 @@ welcome = "Welcome to the programming diary !"
 #     {"content": "Blablablabla", "date":"04-01-2023"},
 # ]
 
+def prompt_new_entry():
+    entry_content = input("What have you learn today ?")
+    entry_date = input("Enter the date: ")
+    add_entry(entry_content, entry_date)
+    
+def view_entries(entries):
+    for entry in entries: 
+            print(f"{entry['date']}\n{entry['content']}\n\n")
 
 while user_input (user_input := input(menu)) != "3":
     #content 
     if user_input == "1":
-        entry_content = input("What have you learn today ?")
-        entry_date = input("Enter the date: ")
-        add_entry(entry_content, entry_date)
+        prompt_new_entry()
     elif user_input == "2":
-        entries = get_entries()
-        for entry in entries: 
-            print(f"{entry['date']}\n{entry['content']}\n\n")
+        view_entries(get_entries())
     else: 
         print("Invalid option, please try again")
 
